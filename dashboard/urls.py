a5de00ebca33ade34fd6cc1e2e152845985c580b
Ferdy <lapizlazuli238@gmail.com>
2026-08-13 02:52:02 +0700

    Fix URL potensi berita dan RT RW

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("potensi/slug:slug/", views.potensi_detail, name="potensi_detail"),
-    path("berita/slug:slug/", views.berita_detail, name="berita_detail"),
+
+    path("potensi/<slug:slug>/", views.potensi_detail, name="potensi_detail"),
+
+    path("berita/<slug:slug>/", views.berita_detail, name="berita_detail"),
+
     path("rt-rw/", views.rt_rw, name="rt_rw"),
 ]
